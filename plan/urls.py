e5e3c36e3c24cb5a99c18398e880dd05198c403d
Kay212MD <kayschreiber26@web.de>
2020-05-01 13:58:41 +0200

    Create urls.py

---
+++ plan/urls.py
@@ -0,0 +1,18 @@
+"""Defines URL patterns for plan"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'plan' 
+urlpatterns = [
+    # Home page
+    path('', views.index, name = 'index'),
+    # Page that will show the plans
+    path('personal_plans', views.personal_plans, name = 'personal_plans'),
+    # Page that will show all food labels of a single Plan
+    path('personal_plans/<int:personal_plan_id>/', views.personal_plan, 
+        name='personal_plan'),
+    # Page for adding a new plan
+    path('new_personal_plan', views.new_personal_plan, name='new_personal_plan'),
+    ]
