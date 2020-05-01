3ae590f9c0a009481f4172167a0eba4f7a2c819a
Kay212MD <kayschreiber26@web.de>
2020-05-01 13:59:08 +0200

    Create admin.py

---
+++ plan/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import PersonalPlan, FoodMainInformation
+
+admin.site.register(PersonalPlan)
+admin.site.register(FoodMainInformation)
+
+# Register your models here.
