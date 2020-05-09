ea0a22a0de3d36643cf83234934ea2c7f686159b
Kay212MD <kayschreiber26@web.de>
2020-05-09 19:51:37 +0200

    Update forms.py

--- plan/forms.py
+++ plan/forms.py
@@ -1,8 +1,23 @@
 from django import forms
-from .models import PersonalPlan
+from .models import PersonalPlan, FoodMainInformation
 
 class PersonalPlanForm(forms.ModelForm):
     class Meta:
         model = PersonalPlan
         fields = ['plan_name']
         labels = {'plan_name':''}
+
+class FoodMainInformationForm(forms.ModelForm):
+    class Meta:
+        model = FoodMainInformation
+        fields = ['food_label',
+                    'food_description',
+                    'food_quantity_necessary',
+                    'food_reserve_necessary', 
+                    'maximum_distribution_of_food']
+        labels = {'food_label':'food_label', 
+                    'food_description':'food_description',
+                    'food_quantity_necessary':'food_quantity_necessary',
+                    'food_reserve_necessary':'food_reserve_necessary',
+                    'maximum_distribution_of_food':'maximum_distribution_of_food'}
+        
